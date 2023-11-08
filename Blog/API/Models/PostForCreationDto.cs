855daf930f667c57c91a7e4f3301ae2a5aefa4c6
nsi4818 <nsi4818@students.fhv.at>
2023-11-08 11:54:53 +0100

    added validation for post creation

--- Blog/API/Models/PostForCreationDto.cs
+++ Blog/API/Models/PostForCreationDto.cs
@@ -1,7 +1,13 @@
-﻿namespace Blog.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.API.Models;
 
 public class PostForCreationDto
 {
+    [Required(ErrorMessage = "You should provide a name value!")]
+    [MaxLength(100)]
     public string Title { get; set; } = string.Empty;
+    [Required(ErrorMessage = "You should provide a content value!")]
+    [MaxLength(256)]
     public string Content { get; set; } = string.Empty;
 }
